c772e15246ccda811cc14187e0e49014d93d980f
Vladimir Gusev <vladimir@gusev.pro>
2018-01-17 20:55:08 +0300

    Update test.sh
    ubuntu httpd restart: increasing sleep intervals

--- test/test.sh
+++ test/test.sh
@@ -38,10 +38,10 @@ function restart_httpd() {
       #systemctl restart apache2
       ## workaround:
       apache2 -k stop
-      sleep 2
+      sleep 3
       pkill   apache2 2>&1 > /dev/null
       apache2 -k start
-      sleep 2
+      sleep 3
     ;;
     *FreeBSD*)
       service apache24 restart
