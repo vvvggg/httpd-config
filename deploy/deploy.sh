721f653609fd12938335f53c830709b1718ee1ed
Vladimir Gusev <vladimir@gusev.pro>
2018-01-23 17:28:30 +0300

    fix deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -243,14 +243,14 @@ esac
 # backing up with '.ORIG' files
 httpd_ver=`httpd -v | grep version | cut -d ' ' -f 3 | cut -d '/' -f 2`
 if [[ $httpd_ver < '2.4.0' ]]; then
- matched_files=`egrep  -RIils                                          \
-                       '^[[:space:]]*SSLSessionTickets[[:space:]]+off' \
+  matched_files=`egrep  -RIils                                          \
+                       '^[[:space:]]*SSLSessionTickets[[:space:]]+off'  \
                        "${conf_predir}/${conf_dir}"/*`
-#   for file in matched_files; do
+  for file in matched_files; do
 #     sed -i.ORIG -r                                                \
 #     -e "s%^([[:space:]]*SSLSessionTickets[[:space:]]+off.*)%#\1%" \
 #     "$file"
-#   done
+  done
 fi
 ## /Post-scripts
 
